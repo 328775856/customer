115fbaec05d395cc5809ebc7962defea66876d7a
sunhao <sh@geeboo.cn>
2018-12-07 14:34:37 +0800

    调整title

--- .webpackrc.js
+++ .webpackrc.js
@@ -3,7 +3,7 @@ export default {
     '/': {
       //  target: 'http://192.168.10.212:8765/api/GbManager_wlj/GbManager',
       // target: 'http://192.168.10.173:8765/api/GbManager_shenwei/GbManager',
-      // target: 'http://192.168.10.173:8765/api/GbManager/GbManager',
+      target: 'http://192.168.10.173:8765/api/GbManager/GbManager',
       target: 'http://192.168.10.173:8768/api',
       // target: 'http://192.168.10.173:8765/api/GbManager_wlj/GbManager',
       changeOrigin: true,
